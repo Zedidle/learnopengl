6cb42048ec63887e23cc45eb10a4f0ecba74c67b
zfc <476409251@qq.com>
2020-05-03 16:41:07 +0800

    Camera

--- FirstProject/FirstProject/main.cpp
+++ FirstProject/FirstProject/main.cpp
@@ -1,12 +1,14 @@
 //#include "Shader.h" // 着色器
 //#include "Texture.h"		// 纹理
 //#include "Transformation.h"	// 变换
-#include "CoordianteSystems.h"		// 坐标系统
+//#include "CoordianteSystems.h"		// 坐标系统
+#include "Camera.h"
 
 int main() {
 	//RunShader();
 	//RunTexture();
 	//RunTransformation();
-	RunCoordinateSystems();
+	//RunCoordinateSystems();
+	RunCamera();
 	return 0;
 }
